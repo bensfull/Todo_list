af9645e6a4fa03d2154a852d505d3330ae4bbad3
Benson <Benson-Pierre@outlook.com>
2025-09-18 10:40:38 -0300

    primeiro commit

---
+++ ToDos/views.py
@@ -0,0 +1,28 @@
+# from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+
+from .models import Todo
+
+# def todo_list(request):
+#     todos= Todo.objects.all()
+#     return render(request, "ToDos/todo_list.html", {"todos": todos})
+
+class TodoListView(ListView):
+    model = Todo
+    
+    
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+    
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url= reverse_lazy("todo_list")
+    
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url= reverse_lazy("todo_list")
